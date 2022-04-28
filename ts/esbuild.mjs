e227e1657499d80b8795bedcd54a0ceffca933e3
hikaru-y <hkrysg@gmail.com>
2022-04-28 23:02:48 +0900

    build: update TS deps and scripts

--- ts/esbuild.mjs
+++ ts/esbuild.mjs
@@ -1,55 +1,55 @@
-import esbuild from 'esbuild';
-import esbuildSvelte from 'esbuild-svelte';
-import * as fs from 'fs/promises';
-import sveltePreprocess from 'svelte-preprocess';
+import esbuild from "esbuild";
+import esbuildSvelte from "esbuild-svelte";
+import * as fs from "fs/promises";
+import sveltePreprocess from "svelte-preprocess";
 
-const outdir = '../dist/web';
+const outdir = "../dist/web";
 
 /** @type {import('esbuild').BuildOptions} */
 const options = {
-	entryPoints: ['src/index.ts'],
-	bundle: true,
-	mainFields: ['svelte', 'browser', 'module', 'main'],
-	minify: true,
-	sourcemap: true,
-	// support Qt 5.14
-	target: ['chrome77'],
-	outdir,
-	external: ['anki'],
-	plugins: [
-		esbuildSvelte({
-			preprocess: sveltePreprocess({ scss: {} }),
-			// https://github.com/EMH333/esbuild-svelte#css-output
-			compilerOptions: { css: true }
-		})
-		// sassPlugin(),
-	],
-	loader: {
-		'.png': 'dataurl',
-		'.svg': 'text'
-	}
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    mainFields: ["svelte", "browser", "module", "main"],
+    minify: true,
+    treeShaking: true,
+    sourcemap: true,
+    // support Qt 5.14
+    target: ["chrome77"],
+    outdir,
+    external: ["anki", "svelte"],
+    plugins: [
+        esbuildSvelte({
+            preprocess: sveltePreprocess({ scss: {} }),
+            // https://github.com/EMH333/esbuild-svelte#css-output
+            compilerOptions: { css: true },
+        }),
+    ],
+    loader: {
+        ".png": "dataurl",
+        ".svg": "text",
+    },
 };
 
-if (process.env.WATCH === 'true') {
-	options.watch = {
-		onRebuild(error, result) {
-			if (error) {
-				console.error('watch build failed:', error);
-			} else {
-				console.log('watch build succeeded:', result);
-			}
-		}
-	};
+if (process.env.WATCH === "true") {
+    options.watch = {
+        onRebuild(error, result) {
+            if (error) {
+                console.error("watch build failed:", error);
+            } else {
+                console.log("watch build succeeded:", result);
+            }
+        },
+    };
 }
 
 fs.rm(outdir, { recursive: true, force: true })
-	.then(() => console.log(`successfully deleted ${outdir}`))
-	.catch((error) => console.error('there was an error:', error.message));
+    .then(() => console.log(`successfully deleted ${outdir}`))
+    .catch((error) => console.error("there was an error:", error.message));
 
 esbuild
-	.build(options)
-	.then((result) => console.log('build succeeded:', result))
-	.catch((error) => {
-		process.stderr.write(error.stderr);
-		process.exit(1);
-	});
+    .build(options)
+    .then((result) => console.log("build succeeded:", result))
+    .catch((error) => {
+        process.stderr.write(error.stderr);
+        process.exit(1);
+    });
